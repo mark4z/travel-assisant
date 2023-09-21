4b651e5355692071d0bfc7e4e4f70ad307a72c37
Mengchao.Lv1 <Mengchao.Lv1@a.c>
2023-09-21 10:02:15 +0800

    init

--- travel.go
+++ travel.go
@@ -9,11 +9,17 @@ import (
 	"strings"
 )
 
-var client http.Client
-var indexUrl = "https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc&fs=#{from-zh},#{from}&ts=#{to-zh},#{to}&date=#{date}&flag=N,N,Y"
-var trainUrl = "https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date=#{date}&leftTicketDTO.from_station=#{from}&leftTicketDTO.to_station=#{to}&purpose_codes=ADULT"
-var mapperUrl = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9274"
-var passUrl = "https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=#{id}&from_station_telecode=#{from}&to_station_telecode=#{to}&depart_date=#{date}"
+var (
+	indexUrl  = "https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc&fs=%s,%s&ts=%s,%s&date=%s&flag=N,N,Y"
+	trainUrl  = "https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT"
+	mapperUrl = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9274"
+	passUrl   = "https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=%s&from_station_telecode=%s&to_station_telecode=%s&depart_date=%s"
+)
+
+var (
+	client http.Client
+	m      map[string]string
+)
 
 func init() {
 	jar, _ := cookiejar.New(nil)
@@ -23,52 +29,40 @@ func init() {
 }
 
 func main() {
-	m := mapper()
-	fromZh := "郑州"
-	from := m[fromZh]
-	toZh := "长治"
-	to := m[toZh]
-	date := "2023-10-01"
+	m = mapper()
+
 	no := "G3133"
-	index(from, fromZh, to, toZh, date)
-	id := train(no, from, to, date, m)
-	stations := pass(id, from, to, date)
+	date := "2023-10-01"
+	fromZh := "郑州"
+	toZh := "长治"
+	index(m[fromZh], fromZh, m[toZh], toZh, date)
+	id := train(no, m[fromZh], m[toZh], date)
+	stations := pass(id, m[fromZh], m[toZh], date)
 	for i := len(stations) - 1; i > 0; i-- {
-		train(no, m[stations[0]], m[stations[i]], date, m)
+		train(no, m[stations[0]], m[stations[i]], date)
 	}
 	for i := 0; i < len(stations)-1; i++ {
-		train(no, m[stations[i]], m[stations[len(stations)-1]], date, m)
+		train(no, m[stations[i]], m[stations[len(stations)-1]], date)
 	}
 }
 
 func index(from, fromZh, to, toZh, date string) {
-	target := indexUrl
-	target = strings.ReplaceAll(target, "#{from-zh}", fromZh)
-	target = strings.ReplaceAll(target, "#{from}", from)
-	target = strings.ReplaceAll(target, "#{to-zh}", toZh)
-	target = strings.ReplaceAll(target, "#{to}", to)
-	target = strings.ReplaceAll(target, "#{date}", date)
-	res, err := client.Get(target)
+	res, err := client.Get(fmt.Sprintf(indexUrl, fromZh, from, toZh, to, date))
 	if err != nil {
 		panic(err)
 	}
-	res.Body.Close()
+	_ = res.Body.Close()
 }
 
-func train(wantNo, from, to, date string, m map[string]string) string {
-	target := trainUrl
-	target = strings.ReplaceAll(target, "#{from}", from)
-	target = strings.ReplaceAll(target, "#{to}", to)
-	target = strings.ReplaceAll(target, "#{date}", date)
-	resp, err := client.Get(target)
+func train(wantNo, from, to, date string) string {
+	resp, err := client.Get(fmt.Sprintf(trainUrl, from, to, date))
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
 	jsonStr, err := io.ReadAll(resp.Body)
-
-	i := &Info{}
-	err = json.Unmarshal(jsonStr, i)
+	_ = resp.Body.Close()
+	info := &Info{}
+	err = json.Unmarshal(jsonStr, info)
 	if err != nil {
 		panic(err)
 	}
@@ -77,7 +71,7 @@ func train(wantNo, from, to, date string, m map[string]string) string {
 		return infos[2], infos[3], infos[32], infos[31], infos[30]
 	}
 
-	for _, v := range i.Data.Result {
+	for _, v := range info.Data.Result {
 		id, no, s, o, t := extraInfo(v)
 		if no == wantNo {
 			fmt.Println(fmt.Sprintf("train :%s [%s:%s] \ttwo：%s\tone：%s\tspecial：%s\tid:%s\t\n  ", no, m[from], m[to], s, o, t, id))
@@ -88,19 +82,15 @@ func train(wantNo, from, to, date string, m map[string]string) string {
 }
 
 func pass(id, from, to, date string) []string {
-	target := passUrl
-	target = strings.ReplaceAll(target, "#{id}", id)
-	target = strings.ReplaceAll(target, "#{from}", from)
-	target = strings.ReplaceAll(target, "#{to}", to)
-	target = strings.ReplaceAll(target, "#{date}", date)
-	res, err := client.Get(target)
+	res, err := client.Get(fmt.Sprintf(passUrl, id, from, to, date))
 	if err != nil {
 		panic(err)
 	}
 	body, err := io.ReadAll(res.Body)
+	_ = res.Body.Close()
 	interval := &IntervalInfo{}
 	_ = json.Unmarshal(body, interval)
-	p := []string{}
+	var p []string
 	for i := range interval.Data.Data {
 		p = append(p, interval.Data.Data[i].StationName)
 	}
@@ -113,12 +103,14 @@ func mapper() map[string]string {
 		panic(err)
 	}
 	body, err := io.ReadAll(response.Body)
+	_ = response.Body.Close()
+
 	resp := string(body)
-	citys := strings.Split(resp, "@")
-	citys = citys[1:]
-	m := make(map[string]string, len(citys)*2)
-	for c := range citys {
-		fields := strings.Split(citys[c], "|")
+	city := strings.Split(resp, "@")
+	city = city[1:]
+	m := make(map[string]string, len(city)*2)
+	for c := range city {
+		fields := strings.Split(city[c], "|")
 		m[fields[1]] = fields[2]
 		m[fields[2]] = fields[1]
 	}
